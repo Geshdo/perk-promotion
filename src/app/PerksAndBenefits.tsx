e7a2700209788ecd7ee740f9898f0d54311c198e
Dino Ferizovic <dinoferizovic@gmail.com>
2024-12-14 11:46:54 +0100

    Adds styling to perks boxes

---
+++ src/app/PerksAndBenefits.tsx
@@ -0,0 +1,73 @@
+import {
+  BubbleChatNotificationIcon,
+  ComputerProgramming02Icon,
+  Brain01Icon,
+  WorkoutWarmUpIcon,
+  ElearningExchangeIcon,
+  UserGroupIcon,
+  Activity03Icon,
+  ViewOffSlashIcon,
+  Target01Icon,
+} from "hugeicons-react";
+
+const ICON_SIZE = 60;
+
+export const perksData = [
+  {
+    icon: <ElearningExchangeIcon color="#5e3a9d" size={ICON_SIZE} />,
+    title: "Education",
+    description:
+      "Your continued education is vital and you will have your own educational plan that you follow up on with your leader frequently to ensure that you are thriving with us.",
+  },
+  {
+    icon: <Brain01Icon color="#5e3a9d" size={ICON_SIZE} />,
+    title: "Well-Being",
+    description:
+      "To take care of your mental health we will provide the possibility to talk to our career coach monthly, to unlock your full potential and ensure your well-being.",
+  },
+  {
+    icon: <ViewOffSlashIcon color="#5e3a9d" size={ICON_SIZE} />,
+    title: "Transparency",
+    description:
+      "We are an open book, nothing is secret and anyone wanting to be part of the decision making process is welcomed with open arms.",
+  },
+  {
+    icon: <BubbleChatNotificationIcon color="#5e3a9d" size={ICON_SIZE} />,
+    title: "Social Life",
+    description:
+      "The work-life balance is key to us. Meetings are during business hours, and overtime is something we always try to avoid.",
+  },
+  {
+    icon: <ComputerProgramming02Icon color="#5e3a9d" size={ICON_SIZE} />,
+    title: "Gear",
+    description:
+      "You need the best tools to be the best, and we will provide you with everything you need to do your best work.",
+  },
+
+  {
+    icon: <WorkoutWarmUpIcon color="#5e3a9d" size={ICON_SIZE} />,
+    title: "Exercise",
+    description:
+      "Are you into sports and think that the cost of golf balls or gymnastics socks are too expensive after you spent all your money on Bitcoin? Don't worry we will provide you with a wellness grant.",
+  },
+
+  {
+    icon: <UserGroupIcon color="#5e3a9d" size={ICON_SIZE} />,
+    title: "Conferences",
+    description:
+      "We organize conferences two times a year where we spend time together to learn and to bond.",
+  },
+  {
+    icon: <Activity03Icon color="#5e3a9d" size={ICON_SIZE} />,
+    title: "Activities",
+    description:
+      "We like spending time together and will therefore organize everything from traditional Aws to Hackathons, quizzes, game nights etc. regularly.",
+  },
+
+  {
+    icon: <Target01Icon color="#5e3a9d" size={ICON_SIZE} />,
+    title: "Targeted Goals",
+    description:
+      "Outside of your education plan you also have targeted goals that you follow up on with your leader and coach regularly. Making sure that you can become the best version of you!",
+  },
+];
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,41 @@
+import { perksData } from "./PerksAndBenefits";
 
 export default function Home() {
-  return <div className="m-auto max-w-screen-md">
+  return (
+    <>
+      <div className="relative h-[450px] ">
+        <h1 className="text-4xl underline-offset-4 transform -translate-x-1/2 decoration-purple-700 underline flex items-center absolute bottom-4 left-1/2 text-white font-semibold my-8 text-center">
+          What can we offer?
+        </h1>
+        <img
+          src={"/group_photo.jpg"}
+          alt="group photo"
+          className="absolute -z-20 h-full contrast-125 top-0 object-cover w-full"
+        />
 
-    <h1>PERK PROMOTION</h1>
-
-    </div>;
+        <div className="absolute -z-10 bottom-0 right-0 left-0 inset-0 bg-gradient-to-b from-transparent to-neutral-950 opacity-75" />
+      </div>
+      <h3 className="text-lg my-24 text-center max-w-md m-auto">
+        If working with the best people, with the greatest technology is not
+        enough we also like to highlight some of our employees most{" "}
+        <mark className="inline-block bg-purple-100">appriciated perks.</mark>
+      </h3>
+      <div className="m-auto max-w-screen-md">
+        <div className="grid grid-cols-3 gap-4 mt-8 gap-y-12 mb-20">
+          {perksData.map((perk, index) => (
+            <div
+              key={index}
+              className="duration-300 rounded-xl hover:transition-transform hover:duration-300 hover:ease-in-out cursor-grab hover:scale-105 flex flex-col justify-center relative border border-slate-200 h-[280px] p-6"
+            >
+              <div className="bg-white h-12 flex justify-center w-12 absolute -top-8 left-1/2 transform -translate-x-1/2">
+                {perk.icon}
+              </div>
+              <h2 className="font-bold text-xl mt-8 mb-2">{perk.title}</h2>
+              <p className="text-sm">{perk.description}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
