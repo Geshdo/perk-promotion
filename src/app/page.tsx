d0a9688b71b41c1d47ab35b32ee8c6ff8dd6b6e7
Dino Ferizovic <dinoferizovic@gmail.com>
2024-12-15 08:48:27 +0100

    Styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { perksData } from "./PerksAndBenefits";
+import { ArrowRight02Icon } from "hugeicons-react";
 
 export default function Home() {
   return (
@@ -15,21 +16,21 @@ export default function Home() {
 
         <div className="absolute -z-10 bottom-0 right-0 left-0 inset-0 bg-gradient-to-b from-transparent to-neutral-950 opacity-75" />
       </div>
-      <h3 className="text-lg my-24 text-center max-w-md m-auto">
+      <p className="text-lg my-24 text-center max-w-md m-auto p-4">
         If working with the best people, with the greatest technology is not
         enough we also like to highlight some of our employees most{" "}
         <mark className="inline-block bg-purple-100 px-2">
           appriciated perks.
         </mark>
-      </h3>
-      <div className="m-auto max-w-[1000px] p-4">
+      </p>
+      <div className="m-auto max-w-[1000px] p-4 mb-12">
         <div className="flex flex-wrap justify-center gap-8 mt-8 gap-y-16 mb-20">
           {perksData.map((perk, index) => (
             <div
               key={index}
-              className="duration-300 w-[250px] rounded-xl hover:transition-transform hover:duration-300 hover:ease-in-out cursor-grab hover:scale-105 flex flex-col justify-center relative border border-slate-200 h-[280px] p-6"
+              className="duration-300 w-[250px] rounded-xl hover:bg-slate-50 hover:transition-transform hover:duration-300 hover:ease-in-out cursor-grab hover:scale-105 flex flex-col justify-center relative border border-slate-200 h-[280px] p-6"
             >
-              <div className="bg-white h-12 flex justify-center w-12 absolute -top-8 left-1/2 transform -translate-x-1/2">
+              <div className=" h-12 flex justify-center w-12 absolute -top-8 left-1/2 transform -translate-x-1/2">
                 {perk.icon}
               </div>
               <h2 className="font-bold text-xl mt-8 mb-2">{perk.title}</h2>
